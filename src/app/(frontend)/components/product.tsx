0d240db394c3658f6f3a3ba56f33fb3c1c3300c7
code with harry <aashish.kumar@96672gmail.com>
2025-12-10 21:39:00 +0530

    first commit

--- src/app/(frontend)/components/product.tsx
+++ src/app/(frontend)/components/product.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 
     return (
         <div className=" mx-auto max-w-[1200px] p-10">
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5">
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
             {docs.map((item: any) => (
                 <div key={item.id} className="w-80 shadow  rounded-lg">
                     {item?.image?.url && (
